762c0f057b6c0ac7217082be74581f124fad42c0
Derek Foulk <derekfoulk@gmail.com>
2023-10-09 10:59:33 -0700

    Temp

---
+++ BlazorRecipes/Client/Services/RecipeService.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http.Json;
+using BlazorRecipes.Shared.Recipes;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+namespace BlazorRecipes.Client.Services
+{
+    public class RecipeService
+    {
+        private readonly HttpClient _httpClient;
+
+        public RecipeService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<Recipe>?> GetAllRecipes()
+        {
+            var recipes = await _httpClient.GetFromJsonAsync<Recipe[]>("Recipes");
+            
+            return recipes;
+        }
+    }
+}
---
+++ Tests/BlazorRecipes.Client.UnitTests/Services/RecipeServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using BlazorRecipes.Client.Services;
+
+namespace BlazorRecipes.Client.UnitTests.Services
+{
+    public class RecipeServiceTest
+    {
+        [Fact]
+        public void GetAllRecipes_ReturnsArrayOfRecipes()
+        {
+            // Arrange
+            RecipeService
+            // Act
+            // Assert
+        }
+    }
+}
